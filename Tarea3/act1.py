f9654e01796ff29807d415dfcdb9764bd9490b16
Loren <loren.chialvo@gmail.com>
2024-04-12 11:57:09 -0300

    3.0.1

--- Tarea3/act1.py
+++ Tarea3/act1.py
@@ -1,12 +1,17 @@
-class cargasum():
-    number1 = input(int("Ingrese el primer numero"))
-    number2 = input(int"Ingrese el siguiente numero")
+def cargasum():
+    try:
+        while True:
+            number1 = input(int("Ingrese el primer numero"))
+            number2 = input(int"Ingrese el siguiente numero")
+            result = number1 + number2
+            print("Su suma es ", result)
+    except ValueError:
+        print("Error! Deben ser numeros enteros")
+    finally:
+        seguir = input("¿Quieres seguir sumando valores? (s/n): ")
+        if seguir.lower() != 's':
+            exit
+        elif seguir.lower() == "s":
+            pass
 
-    def plus(num1, num2):
-        result = num1 + num2
-        return "el resultado es ", result
-    
-    plus(number1, number2)
-
-    print("Desea seguir poniendo numeros? (si = y | no = n)")
-    wish = input(str(">>>"))
+cargasum()
