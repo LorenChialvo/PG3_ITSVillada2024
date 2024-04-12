8bee372b9337f23eca5ef1f201cf077856bf3abb
Loren <loren.chialvo@gmail.com>
2024-04-12 11:56:36 -0300

    3.0.1

---
+++ Tarea3/act1.py
@@ -0,0 +1,12 @@
+class cargasum():
+    number1 = input(int("Ingrese el primer numero"))
+    number2 = input(int"Ingrese el siguiente numero")
+
+    def plus(num1, num2):
+        result = num1 + num2
+        return "el resultado es ", result
+    
+    plus(number1, number2)
+
+    print("Desea seguir poniendo numeros? (si = y | no = n)")
+    wish = input(str(">>>"))
